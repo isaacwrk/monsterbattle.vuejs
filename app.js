bb25ddcefa23a26f592d9a54d38fccd33e437626
isaacwrk <isaacsouzawrk@gmail.com>
2021-07-21 17:58:07 -0300

    inital commit

---
+++ app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el:"#app",
+    data:{
+        playerLife:10,
+        monsterLife:0,
+        
+    },
+    computed:{
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        
+    },
+    watch:{
+
+    }
+})
