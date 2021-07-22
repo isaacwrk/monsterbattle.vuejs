a963feefe2609fa4af6a603e9b1097941d1a7f5d
isaacwrk <isaacsouzawrk@gmail.com>
2021-07-21 22:27:54 -0300

    construindo as açoes de atacar

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ new Vue({
     el:"#app",
     data:{
         running:false,
-        playerLife:10,
-        monsterLife:0,
+        playerLife:100,
+        monsterLife:100,
         
     },
     computed:{
@@ -12,9 +12,30 @@ new Vue({
         }
     },
     methods: {
-        
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+    },
+    attack(especial){
+        this.hurt('playerLife',7,12,false)
+        this.hurt('monsterLife',5,10,especial)
+    },
+    hurt(atr,min,max,especial){
+        const plus = especial ? 5 : 0 
+        const hurt = this.getRandom(min + plus, max + plus)
+        //evitando que o o life do jogador fique negativo, sendo que o valor minimo aceitável seja 0
+        this[atr] = Math.max(this.playerLife - hurt, 0)
+    },
+    getRandom(min,max){
+        //gerando um valor entre 0 1 multiplicando o valor pelo máximo menos o minimo
+        const value = Math.random() * (max - min) + min
+        return Math.round(value)
+        }
     },
     watch:{
-
+        hasResult(value){
+            if(value) this.running = false
+        }
     }
 })
